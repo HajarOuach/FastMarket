aa3524972225c6257afbd4ee6623dc751fea1b2e
hajarO <hajar.ouach.15@gmail.com>
2025-05-20 00:11:21 +0200

    Ajout button 2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
       <h1>Bienvenue sur mon site !</h1>
       <p>Ce site est construit avec React.</p>
       <button>Hajar</button>
+      <button>Hajar2</button>
     </div>
   );
 }
