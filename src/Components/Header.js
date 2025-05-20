9ed88594011db1c695a1bd3cf12c0bf802d2319a
hajarO <hajar.ouach.15@gmail.com>
2025-05-20 11:23:13 +0200

    ajout bootstrap

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,139 +1,74 @@
 import React from 'react';
+import { Container, Row, Col, Form, FormControl, Button } from 'react-bootstrap';
 
 function Header() {
   return (
-    <>
-      {/* Cart Offcanvas */}
-      <div className="offcanvas offcanvas-end" data-bs-scroll="true" tabIndex="-1" id="offcanvasCart" aria-labelledby="My Cart">
-        <div className="offcanvas-header justify-content-center">
-          <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-        </div>
-        <div className="offcanvas-body">
-          <div className="order-md-last">
-            <h4 className="d-flex justify-content-between align-items-center mb-3">
-              <span className="text-primary">Your cart</span>
-              <span className="badge bg-primary rounded-pill">3</span>
-            </h4>
-            <ul className="list-group mb-3">
-              <li className="list-group-item d-flex justify-content-between lh-sm">
-                <div>
-                  <h6 className="my-0">Growers cider</h6>
-                  <small className="text-body-secondary">Brief description</small>
-                </div>
-                <span className="text-body-secondary">$12</span>
-              </li>
-              <li className="list-group-item d-flex justify-content-between lh-sm">
-                <div>
-                  <h6 className="my-0">Fresh grapes</h6>
-                  <small className="text-body-secondary">Brief description</small>
-                </div>
-                <span className="text-body-secondary">$8</span>
-              </li>
-              <li className="list-group-item d-flex justify-content-between lh-sm">
-                <div>
-                  <h6 className="my-0">Heinz tomato ketchup</h6>
-                  <small className="text-body-secondary">Brief description</small>
-                </div>
-                <span className="text-body-secondary">$5</span>
-              </li>
-              <li className="list-group-item d-flex justify-content-between">
-                <span>Total (USD)</span>
-                <strong>$20</strong>
-              </li>
-            </ul>
+    <header className="py-3 border-bottom bg-white">
+      <Container fluid>
+        <Row className="align-items-center">
+          {/* LOGO + TOGGLER */}
+          <Col xs={4} lg={2} className="d-flex align-items-center gap-3 justify-content-start">
+            <img src="images/logo.svg" alt="Organic Logo" style={{ height: 32 }} />
+            <button className="btn p-0 border-0" type="button">
+              <svg width="24" height="24" viewBox="0 0 24 24">
+                <use xlinkHref="#menu" />
+              </svg>
+            </button>
+          </Col>
 
-            <button className="w-100 btn btn-primary btn-lg" type="submit">Continue to checkout</button>
-          </div>
-        </div>
-      </div>
+          {/* SEARCH BAR */}
+          <Col xs={12} md={6} lg={5}>
+            <Form className="d-flex bg-light rounded-4 p-2">
+              <Form.Select className="border-0 bg-transparent" style={{ maxWidth: 150 }}>
+                <option>All Categories</option>
+                <option>Groceries</option>
+                <option>Drinks</option>
+              </Form.Select>
+              <FormControl
+                type="search"
+                placeholder="Search for more than 20,000 products"
+                className="me-2 border-0 bg-transparent"
+              />
+              <Button variant="link" className="p-0">
+                <svg width="24" height="24" viewBox="0 0 24 24">
+                  <path
+                    fill="currentColor"
+                    d="M21.71 20.29L18 16.61A9 9 0 1 0 16.61 18l3.68 3.68a1 1 0 0 0 1.42 0a1 1 0 0 0 0-1.39ZM11 18a7 7 0 1 1 7-7a7 7 0 0 1-7 7Z"
+                  />
+                </svg>
+              </Button>
+            </Form>
+          </Col>
 
-      {/* Search Offcanvas */}
-      <div className="offcanvas offcanvas-end" data-bs-scroll="true" tabIndex="-1" id="offcanvasSearch" aria-labelledby="Search">
-        <div className="offcanvas-header justify-content-center">
-          <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-        </div>
-        <div className="offcanvas-body">
-          <div className="order-md-last">
-            <h4 className="d-flex justify-content-between align-items-center mb-3">
-              <span className="text-primary">Search</span>
-            </h4>
-            <form role="search" action="index.html" method="get" className="d-flex mt-3 gap-0">
-              <input className="form-control rounded-start rounded-0 bg-light" type="email" placeholder="What are you looking for?" aria-label="What are you looking for?" />
-              <button className="btn btn-dark rounded-end rounded-0" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-      </div>
-
-      {/* Header */}
-      <header>
-        <div className="container-fluid">
-          <div className="row py-3 border-bottom">
-
-            <div className="col-sm-4 col-lg-3 text-center text-sm-start">
-              <div className="main-logo">
-                <a href="/">
-                  <img src="assets/img/logo.png" alt="logo" className="img-fluid" />
-                </a>
-              </div>
-            </div>
-
-            <div className="col-sm-6 offset-sm-2 offset-md-0 col-lg-5 d-none d-lg-block">
-              <div className="search-bar row bg-light p-2 my-2 rounded-4">
-                <div className="col-md-4 d-none d-md-block">
-                  <select className="form-select border-0 bg-transparent">
-                    <option>All Categories</option>
-                    <option>Groceries</option>
-                    <option>Drinks</option>
-                    <option>Chocolates</option>
-                  </select>
-                </div>
-                <div className="col-11 col-md-7">
-                  <form id="search-form" className="text-center" action="index.html" method="post">
-                    <input type="text" className="form-control border-0 bg-transparent" placeholder="Search for more than 20,000 products" />
-                  </form>
-                </div>
-                <div className="col-1">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                    <path fill="currentColor" d="M21.71 20.29L18 16.61A9 9 0 1 0 16.61 18l3.68 3.68a1 1 0 0 0 1.42 0a1 1 0 0 0 0-1.39ZM11 18a7 7 0 1 1 7-7a7 7 0 0 1-7 7Z" />
-                  </svg>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-sm-8 col-lg-4 d-flex justify-content-end gap-5 align-items-center mt-4 mt-sm-0 justify-content-center justify-content-sm-end">
-              <div className="support-box text-end d-none d-xl-block">
-                <span className="fs-6 text-muted">For Support?</span>
-                <h5 className="mb-0">+980-34984089</h5>
-              </div>
-
-              <ul className="d-flex justify-content-end list-unstyled m-0">
-                <li>
-                  <a href="#" className="rounded-circle bg-light p-2 mx-1">
-                    <svg width="24" height="24" viewBox="0 0 24 24"><use xlinkHref="#user" /></svg>
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="rounded-circle bg-light p-2 mx-1">
-                    <svg width="24" height="24" viewBox="0 0 24 24"><use xlinkHref="#heart" /></svg>
-                  </a>
-                </li>
-                <li className="d-lg-none">
-                  <a href="#" className="rounded-circle bg-light p-2 mx-1" data-bs-toggle="offcanvas" data-bs-target="#offcanvasCart" aria-controls="offcanvasCart">
-                    <svg width="24" height="24" viewBox="0 0 24 24"><use xlinkHref="#cart" /></svg>
-                  </a>
-                </li>
-                <li className="d-lg-none">
-                  <a href="#" className="rounded-circle bg-light p-2 mx-1" data-bs-toggle="offcanvas" data-bs-target="#offcanvasSearch" aria-controls="offcanvasSearch">
-                    <svg width="24" height="24" viewBox="0 0 24 24"><use xlinkHref="#search" /></svg>
-                  </a>
-                </li>
+          {/* NAV LINKS */}
+          <Col md={4} lg={3} className="d-none d-lg-flex justify-content-center gap-4 fw-bold text-uppercase">
+            <a href="#home" className="text-dark text-decoration-none">Home</a>
+            <div className="dropdown">
+              <a className="text-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" href="#pages">
+                Pages
+              </a>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#about">About</a></li>
+                <li><a className="dropdown-item" href="#shop">Shop</a></li>
               </ul>
             </div>
-          </div>
-        </div>
-      </header>
-    </>
+          </Col>
+
+          {/* ICONS */}
+          <Col xs={4} lg={2} className="d-flex justify-content-end gap-3">
+            <a href="#" className="text-dark">
+              <svg width="24" height="24"><use xlinkHref="#user" /></svg>
+            </a>
+            <a href="#" className="text-dark">
+              <svg width="24" height="24"><use xlinkHref="#wishlist" /></svg>
+            </a>
+            <a href="#" className="text-dark">
+              <svg width="24" height="24"><use xlinkHref="#shopping-bag" /></svg>
+            </a>
+          </Col>
+        </Row>
+      </Container>
+    </header>
   );
 }
 
