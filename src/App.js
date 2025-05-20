ec8fd808da0042c3a6a6c6d4eaf481b39ccc8508
mikasa1217 <merabushera@gmail.com>
2025-05-20 10:53:14 +0200

    login local

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-
+import { useState } from 'react';
 import Header from './Components/Header';
 import HeroSection from './Components/HeroSection';
 import BestSellers from './Components/BestSellers';
+import Login from './Components/Login'; // 👈 ajoute ce fichier
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [user, setUser] = useState(null); // 👈 état utilisateur
+
+  const handleLogin = (userData) => {
+    setUser(userData);
+  };
+
   return (
     <div>
-      <Header />
-      <HeroSection />
-            <BestSellers />
+      {!user ? (
+        <Login onLogin={handleLogin} />
+      ) : (
+        <>
+          <Header />
+          <HeroSection />
+          <BestSellers />
+        </>
+      )}
     </div>
   );
 }
+
 export default App;
