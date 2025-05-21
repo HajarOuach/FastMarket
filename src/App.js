6dc6f4a49bc00da2c016340226e08293531c09b8
bnmsanda <sanda.benamane@yahoo.com>
2025-05-21 19:55:11 +0200

    modif app

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 
 import Header from './Components/Header';
 import HeroSection from './Components/HeroSection';
-import BestSellers from './Components/BestSellers';
 import Login from './Components/Login';
 import Catalogue from './Components/Catalogue';
-import Categories from './Components/Categories';
 import Categories from './Components/Categories';
 import BestSellings from './Components/BestSellings';
 import Panier from './Components/Panier';
@@ -23,88 +21,68 @@ function App() {
       setUser(JSON.parse(storedUser));
     }
   }, []);
-  const [user, setUser] = useState(null);
-
-  // Charger l'utilisateur depuis localStorage au démarrage
-  useEffect(() => {
-    const storedUser = localStorage.getItem("client");
-    if (storedUser) {
-      setUser(JSON.parse(storedUser));
-    }
-  }, []);
 
   const handleLogin = (userData) => {
     setUser(userData);
   };
 
   return (
-    
-    
-      <Routes>
-        <Route
-          path="/"
-          element={
-            !user ? (
-              <Login onLogin={handleLogin} />
-            ) : (
-              <Navigate to="/accueil" />
-            )
-          }
-        />
+    <Routes>
+      <Route
+        path="/"
+        element={
+          !user ? (
+            <Login onLogin={handleLogin} />
+          ) : (
+            <Navigate to="/accueil" />
+          )
+        }
+      />
 
-        <Route
-          path="/accueil"
-          element={
-            user ? (
-              <Navigate to="/accueil" />
-            )
-          }
-        />
+      <Route
+        path="/accueil"
+        element={
+          user ? (
+            <>
+              <Header />
+              <HeroSection />
+              <Categories />
+              <BestSellings />
+            </>
+          ) : (
+            <Navigate to="/" />
+          )
+        }
+      />
 
-        <Route
-          path="/accueil"
-          element={
-            user ? (
-              <>
-                <Header />
-                <HeroSection />
-                <BestSellers />
-                <Categories />
-                <BestSellings />
-                <Categories />
-                <BestSellings />
-              </>
-            ) : (
-              <Navigate to="/" />
-            ) : (
-              <Navigate to="/" />
-            )
-          }
-        />
+      <Route
+        path="/catalogue"
+        element={
+          user ? (
+            <>
+              <Header />
+              <Catalogue />
+            </>
+          ) : (
+            <Navigate to="/" />
+          )
+        }
+      />
 
-
-        <Route
-          path="/catalogue"
-          element={user ? <Catalogue /> : <Navigate to="/" />}
-          element={user ? <Catalogue /> : <Navigate to="/" />}
-        />
-
-        <Route
-  path="/panier"
-  element={
-    user ? (
-      <>
-        <Header />
-        <Panier />
-      </>
-    ) : (
-      <Navigate to="/" />
-    )
-  }
-/>
-      </Routes>
-    
-    
+      <Route
+        path="/panier"
+        element={
+          user ? (
+            <>
+              <Header />
+              <Panier />
+            </>
+          ) : (
+            <Navigate to="/" />
+          )
+        }
+      />
+    </Routes>
   );
 }
 
